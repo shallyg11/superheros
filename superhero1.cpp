67cc7a290618b58e5406aad90cba82ef77bd784f
shallyg11 <71091433+shallyg11@users.noreply.github.com>
2022-01-03 23:31:08 +0530

    Add files via upload

---
+++ superhero1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+string getPossibleCharsForKeypad(char c){
+    if (c == '2')
+       return "abc";
+    if (c == '3')
+       return "def";
+    if (c == '4')
+       return "ghi";
+    if (c == '5')
+       return "jkl";
+    if (c == '6')
+       return "mno";
+    if (c == '7')
+       return "pqrs";
+    if (c == '8')
+       return "tuv";
+    if (c == '9')
+       return "wxyz";
+
+    return "";
+}
+int getKeypadCombinations(string str,string output[]){
+    if(str.length()==0){
+        output[0]="";
+        return 1;
+    }
+    int smallerOutputSize=getKeypadCombinations(str.substr(1),output);
+    string firstDigitPossibleChars=getPossibleCharsForKeypad(str[0]);
+
+    string temp[1000];
+    int k=0;
+
+    for(int i=0;i<smallerOutputSize;i++){
+        for(int j=0;j<firstDigitPossibleChars.length();j++){
+            temp[k]=firstDigitPossibleChars[j]+output[i];
+            k++;
+        }
+    }
+    for(int i=0;i<k;i++){
+        output[i]=temp[i];
+    }
+    return smallerOutputSize*firstDigitPossibleChars.length();
+}
+
+int main()
+{
+    string output[1000];
+   
+    int outputSize=getKeypadCombinations("2855",output);
+    
+    for(int i=0;i<outputSize;i++){
+        cout<<output[i]<<endl;
+    }
+    
+} 
+
+
+
+
+
